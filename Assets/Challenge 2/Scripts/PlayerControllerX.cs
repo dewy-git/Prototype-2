6d17e8aa61e05636acd4888d09eb8b585c43ab5a
dewy-git <dewu@s.sfusd.edu>
2023-01-30 14:46:19 -0800

    lesson progress
    completed lesson 9

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,19 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float countAfterPress = 0f;
+    private float scalePressFrequency = 0.9f;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && countAfterPress < 1)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            countAfterPress += 1;
+        }else
+        {
+            countAfterPress -= Time.deltaTime * scalePressFrequency;
         }
     }
 }
