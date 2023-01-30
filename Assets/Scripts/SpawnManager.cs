228a4d06d45a6c53ec91028400fbdea41768f6c3
dewy-git <dewu@s.sfusd.edu>
2023-01-30 14:12:14 -0800

    lesson 2.4 completed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[]animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 15;
     private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
@@ -21,6 +21,8 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
+    
+    // random animal spawner
 
     void SpawnRandomAnimal()
     {
